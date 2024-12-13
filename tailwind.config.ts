ccca814759c6097ab78b8492a991c7fa122c06eb
ysw421 <ysw421@naver.com>
2024-12-14 02:18:39 +0900

    layout setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        dark: {
+          background: 'var(--background-dark)',
+          foreground: 'var(--foreground-dark)',
+        },
       },
     },
   },
