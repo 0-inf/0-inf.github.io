2cbb244f0896a82d6091a1be567ed2251f6c485b
ysw421 <ysw421@naver.com>
2024-12-14 03:44:27 +0900

    adit repo name

--- next.config.ts
+++ next.config.ts
@@ -2,41 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://0-inf.github.io/0-inf-page/' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/0-inf-page' : '',
-  trailingSlash: true,
-  reactStrictMode: true,
-  webpack5: true,
-  eslint: {
-    dirs: ['src'],
-  },
-  experimental: {
-    esmExternals: true,
-  },
-  images: {
-    domains: ['0-inf.github.io/0-inf-page'],
-  },
-  fonts: {
-    domains: ['0-inf.github.io/0-inf-page'],
-  },
-  sassOptions: {
-    prependData: `
-    $base-path: '${process.env.NODE_ENV === 'production' ? '/0-inf-page' : ''}';
-  `,
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' https://0-inf.github.io; script-src 'self' 'unsafe-inline';`,
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
