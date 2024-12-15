0e598eb88061c773a8ff138c440288bdda661e0c
ysw421 <ysw421@naver.com>
2024-12-16 08:31:41 +0900

    fix slug page error

--- src/app/member/[slug]/page.tsx
+++ src/app/member/[slug]/page.tsx
@@ -3,13 +3,16 @@ import Image from 'next/image';
 
 import { GithubBtn, SoundcloudBtn, WebsiteBtn, YoutubeBtn } from '@/components/EmojiBtn';
 import { MemberInfoType, MemberDataType } from '@/type/MemberType';
+import { Metadata } from 'next';
 
-type PageProps<T extends Record<string, string>> = {
-  params: T;
+type Params = {
+  params: {
+    slug: string;
+  };
 };
 
-export default async function Post({ params }: PageProps<{ slug: string }>) {
-  const { slug } = await params;
+export default function Post({ params }: Params) {
+  const { slug } = params;
   const name: string = slug.replace('_', ' ');
 
   const memberData: MemberDataType | null = loadYAML<MemberDataType>('/data/member.yml');
@@ -38,4 +41,24 @@ export default async function Post({ params }: PageProps<{ slug: string }>) {
       )}
     </>
   );
+}
+
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  const name: string = params.slug.replace('_', ' ');
+
+  return {
+    title: `Member: ${name}`,
+    description: `${name}'s Introduction`,
+  };
+}
+
+export async function generateStaticParams() {
+  const memberData: MemberDataType | null = loadYAML<MemberDataType>('/data/member.yml');
+
+  const slugList = [{ slug: '__test__' }];
+  memberData?.members.map((member) => {
+    slugList.push({ slug: member.name.replace(/ /g, '_') });
+  });
+
+  return slugList;
 }
